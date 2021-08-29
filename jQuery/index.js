b679a7ec00492292f3b77e9250f7262c51fa512f
unknown <kalyankarrahul500@gmail.com>
2021-08-29 23:10:29 +0530

    playing with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,15 @@
+//changing color
+$("h1").css("color", "purple");
+
+//add class
+$("h1").addClass("big-title margin-50");
+
+//click event
+$("h1").click(function () {
+    $("h1").css("color", "purple");
+});
+
+//adding keypress
 $("body").keypress(function(event) {
     $("h1").text(event.key);
-})  
+});  
