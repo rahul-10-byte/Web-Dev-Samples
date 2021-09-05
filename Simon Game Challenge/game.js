597bdff59c8a243228f494ba5410a6ae06499d5c
unknown <kalyankarrahul500@gmail.com>
2021-09-05 23:26:56 +0530

    Added jQuery

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -2,6 +2,20 @@ var buttonColours = ["green", "red", "yellow", "blue"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+
+    playSound(userChosenColour);
+
+});
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
 
@@ -12,5 +26,28 @@ function nextSequence() {
     console.log(randomChosenColour);
 
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+
+}
+
+function playSound(name) {
+
+    var audio = new Audio ("sounds/" + name + ".mp3");
+
+    audio.play();
+
+}
+
+function animatePress(currentColour) {
+    
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
 }
 
