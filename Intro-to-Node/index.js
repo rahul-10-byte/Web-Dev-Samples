b14c6640e4e7eff93aa79ba5ad70f5b9eb28597a
unknown <kalyankarrahul500@gmail.com>
2021-09-12 23:08:49 +0530

    getting started with npm

---
+++ Intro-to-Node/index.js
@@ -0,0 +1,9 @@
+var supervillains = require("supervillains");
+var superheroes = require("superheroes");
+
+
+var mySuperVillain = supervillains.random();
+var mySuperHero = superheroes.random();
+
+console.log(mySuperHero);
+console.log(mySuperVillain);
